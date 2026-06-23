705abd08e07f2f64f8b79e93075a984fb0dade7d
kidsjl1106-hash <kidsjl1106@gmail.com>
2026-06-23 09:14:50 +0900

    Point dashboard to owner-run Apps Script deployment

--- release/test/auth-config.js
+++ release/test/auth-config.js
@@ -1,4 +1,4 @@
 window.SNOWLINE_AUTH_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbxWkjPEzw2Si6iIwpudw8yYGHRKQ5N6L1de_1iyfccQ-lpiWlSu5NzFItcgRsCaWT3P/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbxlD1pK91_eovpcfhSt67elcnHZaf6z68vaUgp2NKL3nb40qsMk3wn2Nk5R4y-Z-aYx/exec",
   sessionKey: "snowline_dashboard_session",
 };
